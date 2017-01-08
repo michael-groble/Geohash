4d9936405c1f2073906bc56b3af764fdaaadec38
michael groble <mike@groble.me>
2017-01-08 14:17:46 -0600

    add location tests

---
+++ Tests/GeohashTests/LocationTests.swift
@@ -0,0 +1,27 @@
+//
+//  LocationTests.swift
+//  Geohash
+//
+//  Created by michael groble on 1/8/17.
+//
+//
+
+import XCTest
+@testable import Geohash
+
+class LocationTests: XCTestCase {
+
+  func testDistance() {
+    let a = Location(longitude: -9.10, latitude: 51.5)
+    let b = Location(longitude: -9.11, latitude: 51.6)
+
+    XCTAssertEqualWithAccuracy(a.distanceInMeters(to:  b), 11140.9, accuracy: 0.1)
+  }
+}
+
+extension LocationTests {
+  static var allTests = [
+    ("testDistance", testDistance),
+  ]
+}
+
