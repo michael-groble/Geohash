f570ee2bce92189f8f3464814f67553118283160
Robert Edwards <rcedwards@gmail.com>
2018-08-15 17:16:39 -0400

    Add xcode project and framework/test targets

---
+++ Resources/Geohash.h
@@ -0,0 +1,18 @@
+//
+//  Geohash.h
+//  Geohash
+//
+//  Created by Robert Edwards on 8/15/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Geohash.
+FOUNDATION_EXPORT double GeohashVersionNumber;
+
+//! Project version string for Geohash.
+FOUNDATION_EXPORT const unsigned char GeohashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Geohash/PublicHeader.h>
+
+
