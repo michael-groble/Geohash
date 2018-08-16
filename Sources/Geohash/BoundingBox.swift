ffaaa6aea98cffbe99e2ae7ddab46dc54ba5059d
Robert Edwards <rcedwards@gmail.com>
2018-08-16 11:08:02 -0400

    Replace if statements with guards

--- Sources/Geohash/BoundingBox.swift
+++ Sources/Geohash/BoundingBox.swift
@@ -7,41 +7,39 @@
 //
 
 public class BoundingBox {
-
+    
     public enum Error: Swift.Error {
         case invalidArguments
     }
-
-  public let min: Location
-  public let max: Location
-
-  public init(min: Location, max: Location) throws {
-    guard
-      min.longitude <= max.longitude &&
-      min.latitude  <= max.latitude else {
-        throw Error.invalidArguments
+    
+    public let min: Location
+    public let max: Location
+    
+    public init(min: Location, max: Location) throws {
+        guard min.longitude <= max.longitude,
+            min.latitude  <= max.latitude else {
+                throw Error.invalidArguments
+        }
+        
+        self.min = min
+        self.max = max
     }
-
-    self.min = min
-    self.max = max
-  }
-
-  public func center() -> Location {
-    return Location(
-      longitude: 0.5 * (self.min.longitude + self.max.longitude),
+    
+    public func center() -> Location {
+        return Location(
+            longitude: 0.5 * (self.min.longitude + self.max.longitude),
       latitude:  0.5 * (self.min.latitude + self.max.latitude)
     )
-  }
-
-  /// - todo: support wrap-around at the 180th meridian
-  public func intersects(_ other: BoundingBox) -> Bool {
-    if
-      max.longitude < other.min.longitude ||
-      max.latitude  < other.min.latitude  ||
-      min.longitude > other.max.longitude ||
-      min.latitude  > other.max.latitude {
-      return false
     }
-    return true
-  }
+    
+  /// - todo: support wrap-around at the 180th meridian
+    public func intersects(_ other: BoundingBox) -> Bool {
+        guard max.longitude > other.min.longitude,
+            max.latitude > other.min.latitude,
+            min.longitude < other.max.longitude,
+            min.latitude < other.max.latitude else {
+                return false
+        }
+        return true
+    }
 }
