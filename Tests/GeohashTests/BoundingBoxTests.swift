fc76d3a73ba5fc484eb98055d68c93e8dde75f18
Robert Edwards <rcedwards@gmail.com>
2018-08-16 11:07:03 -0400

    Remove force try in favor of XCTFail or preconditionFailure

--- Tests/GeohashTests/BoundingBoxTests.swift
+++ Tests/GeohashTests/BoundingBoxTests.swift
@@ -10,38 +10,40 @@ import XCTest
 @testable import Geohash
 
 class BoundingBoxTests: XCTestCase {
-
-  var subject: BoundingBox!
-
-  override func setUp() {
-    super.setUp()
-
-    self.subject = try! BoundingBox(
-      min: Location(longitude: -10, latitude: -10),
-      max: Location(longitude: +10, latitude: +10)
-    )
-  }
-
-  func testIntersectsIntersecting() throws {
-    let intersecting = try BoundingBox(
-      min: Location(longitude: -20, latitude: -20),
-      max: Location(longitude:  -9, latitude: -9)
-    )
-    XCTAssertTrue(subject.intersects(intersecting))
-  }
-
-  func testIntersectsNonIntersecting() throws {
-    let intersecting = try BoundingBox(
-      min: Location(longitude: -20, latitude: -20),
-      max: Location(longitude:  -9, latitude: -10.1)
-    )
-    XCTAssertFalse(subject.intersects(intersecting))
-  }
+    
+    var subject: BoundingBox!
+    
+    override func setUp() {
+        super.setUp()
+        do {
+            self.subject = try BoundingBox(
+                min: Location(longitude: -10, latitude: -10),
+                max: Location(longitude: +10, latitude: +10))
+        } catch {
+            XCTFail("Failed to setup test")
+        }
+    }
+    
+    func testIntersectsIntersecting() throws {
+        let intersecting = try BoundingBox(
+            min: Location(longitude: -20, latitude: -20),
+            max: Location(longitude: -9, latitude: -9)
+        )
+        XCTAssertTrue(subject.intersects(intersecting))
+    }
+    
+    func testIntersectsNonIntersecting() throws {
+        let intersecting = try BoundingBox(
+            min: Location(longitude: -20, latitude: -20),
+            max: Location(longitude: -9, latitude: -10.1)
+        )
+        XCTAssertFalse(subject.intersects(intersecting))
+    }
 }
 
 extension BoundingBoxTests {
-  static var allTests = [
-    ("testIntersectsIntersecting", testIntersectsIntersecting),
+    static var allTests = [
+        ("testIntersectsIntersecting", testIntersectsIntersecting),
     ("testIntersectsNonIntersecting", testIntersectsNonIntersecting),
-  ]
+    ]
 }
